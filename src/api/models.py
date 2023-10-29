ec04c6990881f90c882985ccaf044f709a7638cf
Dandead <61425314+Dandead@users.noreply.github.com>
2023-10-29 15:22:42 +0300

    feat!:add custom user model

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+from .managers import CustomUserManager
+from hashlib import sha256
+import uuid
 
 
+def user_media_path(instance, filename) -> str:
+	"""Create user's media path based on email hash"""
+	folder :str = f'user_{sha256(str(instance.email).encode("utf-8"))}'
+	hashed_filename :str = f'{uuid.uuid4}.{str(filename).split(".")[-1]}'
+	return f'{folder}/{hashed_filename}'
+
+# class DatingUser(AbstractBaseUser):
+class DatingUser(AbstractBaseUser, PermissionsMixin):
+	"""Overring basic user model for DatingApp"""
+	email = models.EmailField('Email', unique=True)
+	first_name = models.CharField("First name", max_length=150)
+	last_name = models.CharField("Second name", max_length=150)
+	date_of_birth = models.DateField(null=True)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+	is_superuser = models.BooleanField(default=False)
+	date_joined = models.DateTimeField(default=timezone.now)
+	GENDER_CHOICES = (
+		('M', 'Male'),
+		('F', 'Female'),
+	)
+	gender = models.CharField('Gender', max_length=1, choices=GENDER_CHOICES)
+	image = models.ImageField(upload_to=user_media_path, null=True)
+	
+	objects = CustomUserManager()
+	USERNAME_FIELD = 'email'
+	EMAIL_FIELD = 'email'
+	REQUIRED_FIELDS = ["first_name", "gender"]
+
+	def __repr__(self):
+		return self.email
+	
--- src/config/settings.py
+++ src/config/settings.py
@@ -103,9 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -114,8 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.DatingUser'
