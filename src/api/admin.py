2258c3523269953c79d2c327630c06863f083759
Dandead <61425314+Dandead@users.noreply.github.com>
2023-11-23 16:07:12 +0300

    fix: make user's info output in admin panel more detailed

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import DatingUser
-
-
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = DatingUser
-    list_display = ("email", "image","is_staff", "is_active",)
+    list_display = ("email", "gender","is_staff", "is_active",)
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
-        (None, {"fields": ("email", "password", "image")}),
+        (None, {"fields": ("email", "password", "image", "first_name", "last_name", "gender")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
     )
     add_fieldsets = (
@@ -26,6 +23,4 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ("email",)
     ordering = ("email",)
-
-
 admin.site.register(DatingUser, CustomUserAdmin)
