79c70cbbfb110e8d844e4ed1aa0f3963c621db2a
Dandead <61425314+Dandead@users.noreply.github.com>
2024-11-21 19:07:00 +0300

    fix: remove unused imports

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.fields import related, related_descriptors
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from users.managers import CustomUserManager
